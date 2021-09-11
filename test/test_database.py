978bfb92992e699f0c6c21aa460c770f7e9cb8d7
l_vektor_m <vektor.keita@gmail.com>
2021-09-12 02:25:25 +0900

    新しく追加したシリーズ物DBのテスト

--- test/test_database.py
+++ test/test_database.py
@@ -1,49 +1,33 @@
 #!/home/l_vektor_m/venv3.8/bin/python
 # -*- coding: utf-8 -*-
 
-from models.database import DBPicData
+from models.database import *
 from models.models import *
 
-db = DBPicData()
+db_sdata = DBSeriesData()
+db_pdata = DBPicData()
 
-testdata00 = PictureData()
+sids = [SID_HEADER + SERIES_NONE_ID, 'sid_qpLNxrJ2xoDNwOAm', 'sid_DRWz5VNOz9rrevqO']
+series_title = ['', 'Twitter', 'ツイッター']
 
-PATH1 = '/static/images/nas_pic/SobaCha/EgFQ_giU0AA05rd.jpg'
-TITLE1 = 'test_data_no'
-TAGS1 = {'tag'}
-STAR1 = 3
-INFO1 = 'test'
-testdata01 = PictureData(path=PATH1,
-                         title=TITLE1,
-                         tags=TAGS1,
-                         star=STAR1,
-                         info=INFO1)
+def test_load_series_data_by_id():
+    for i, sid in enumerate(sids):
+        data = db_sdata.load_series_data_by_id(sid)
+        assert data.get_series_title() == series_title[i]
 
-PATH2 = '/static/images/nas_pic/SobaCha/EQspibEVAAAFiIT.jpg'
-TITLE2 = 'シリーズあり'
-TAGS2 = {'tag1', 'タグ2', '3'}
-STAR2 = 5
-INFO2 = 'とても楽しいテスト'
-SERIES_TITLE2 = '楽しいテスト'
-SERIES_PAGE2 = 1
-testdata02 = PictureData(path=PATH2,
-                         title=TITLE2,
-                         tags=TAGS2,
-                         star=STAR2,
-                         info=INFO2,
-                         series_title=SERIES_TITLE2,
-                         series_page=SERIES_PAGE2)
+def test_load_series_data_by_title():
+    for i, title in enumerate(series_title):
+        data = db_sdata.load_series_data_by_title(title)
+        assert data.get_sid() == sids[i]
 
-def test_save_pic_data():
-    db.save_pic_data(testdata00)
-    db.save_pic_data(testdata01)
-    db.save_pic_data(testdata02)
+def test_is_id_in_series():
+    for sid in sids:
+        assert db_sdata.is_id_in_series(sid)
 
-def test_load_pic_data():
-    data00 = db.load_pic_data(testdata00.get_id())
-    assert data00.get_id() == testdata00.get_id()
-    assert data00.get_path() == testdata00.get_path()
-    assert data00.get_title() == testdata00.get_title()
-    assert data00.get_tags() == testdata00.get_tags()
-    assert data00.get_star() == testdata00.get_star()
-    assert data00.get_info() == testdata00.get_info()
+    assert not db_sdata.is_id_in_series(SERIES_NONE_ID)
+
+def test_is_title_in_series():
+    for title in series_title:
+        assert db_sdata.is_title_in_series(title)
+
+    assert not db_sdata.is_title_in_series('title')
